d3c31efd3289d780f7dc635be5cb65bf12257927
C Wiebe <ctwiebe23@gmail.com>
2025-10-06 03:01:01 -0500

    removed in-progress shell site from main

--- NEW/src/main.js
+++
@@ -1,5 +0,0 @@
-import { process } from "@protorobotics/jenga";
-import { definitions } from "./blocks";
-import { pythonGenerator } from "blockly/python";
-
-const toolbox = process(definitions, pythonGenerator)
