87da504f4e50ba9939d3b2c17bf88094158e4880
Kaden Ho <kho7@huskers.unl.edu>
2026-02-04 19:57:39 -0600

    Add column for examples

--- src/builders/BuildCheatSheet.js
+++ src/builders/BuildCheatSheet.js
@@ -6,43 +6,64 @@ import {div, tag, hr} from 'ellipsi'
 import blocks from '../data/blocks'
 
 function getCodeFromBlock(workspace, generator, name) {
-    const block = workspace.newBlock(name)
-    block.initSvg()
-    block.render()
-    const py = generator.blockToCode(block)
-    block.dispose
-    return py
+    const block = workspace.newBlock(name);
+    block.initSvg();
+    block.render();
+    const py = generator.blockToCode(block);
+    block.dispose;
+    return py;
 }
 
 function createEntry(workspace, entry) {
     const entryDiv = div();
-    entryDiv.appendChild(tag("p", `${getCodeFromBlock(workspace, pythonGenerator, entry.name)}`));
-    entryDiv.appendChild(tag("h4", `${entry.description}`));
-    entryDiv.appendChild(hr());
+    entryDiv.appendChild(tag("small", `${getCodeFromBlock(workspace, pythonGenerator, entry.name)} → ${entry.description}`));
     return entryDiv; 
+}
+
+function createCodeDiv(block, workspace) {
+    const codeDiv = div();
+    codeDiv.style.width = "50%";
+    codeDiv.style.padding = "10px";
+    for (let j = 0; j < block.entries.length; j++) {
+        codeDiv.appendChild(createEntry(workspace, block.entries[j]));
+        codeDiv.appendChild(tag("br"));
+    }
+    return codeDiv;
+}
+
+function createExamplesDiv() {
+    const examplesDiv = div();
+    examplesDiv.style.width = "50%";
+    examplesDiv.style.padding = "10px";
+    examplesDiv.style.borderLeft = "dashed 1px";
+    examplesDiv.appendChild(tag("small", "Im div'in here!"));
+    return examplesDiv;
 }
 
 function createSection(workspace, block) {
     const sectionDiv = div();
-    sectionDiv.append(tag("h2", `${block.name}`));
-    for (let j=0; j<block.entries.length; j++) {
-        sectionDiv.appendChild(createEntry(workspace, block.entries[j]));
-    }
+    const sectionHeader = tag("h2", `${block.name}`);
+    sectionHeader.style.borderBottom = "dashed 1px";
+    sectionHeader.style.textAlign = "center";
+    sectionDiv.append(sectionHeader);
+    const contentHolder = div();
+    contentHolder.style.display = "flex"; 
+    contentHolder.appendChild(createCodeDiv(block, workspace));
+    contentHolder.appendChild(createExamplesDiv(block, workspace));
+    sectionDiv.appendChild(contentHolder);
+    sectionDiv.appendChild(hr());
     return sectionDiv;
 }
 
+
 export default function (toolbox) {    
-    const mainDiv = div()
-    const BlocklyCanvas = div({ id: 'block-canvas' })
-    BlocklyCanvas.style.display = "none"
+    const BlocklyCanvas = div({ id: 'block-canvas' });
+    BlocklyCanvas.style.display = "none";
     const workspace = inject(BlocklyCanvas, {
             toolbox: toolbox,
     })
     pythonGenerator.init(workspace);
-    console.log(blocks);
-    // TODO: Refactor this into multiple functions,
-    // Remember to npx webpack before reloading
-    /* python -m http.server -> http://localhost:8000/?cheatsheet*/
+    const mainDiv = div();
     for (let i=0; i<blocks.length; i++) {
         mainDiv.append(createSection(workspace, blocks[i]));
     }
