19feef040925cf3c12e88bd06b097fcb17919664
Kaden Ho <kho7@huskers.unl.edu>
2026-02-04 19:04:21 -0600

    Implement pulling data from blocks and putting it into the cheatsheet

--- src/builders/BuildCheatSheet.js
+++ src/builders/BuildCheatSheet.js
@@ -1,7 +1,50 @@
-import { b, div, tag } from 'ellipsi'
+import {
+    inject
+} from 'blockly'
+import { pythonGenerator } from 'blockly/python'
+import {div, tag, hr} from 'ellipsi'
+import blocks from '../data/blocks'
 
-export default (toolbox, vocab) => {
-    const SomeContent = div('placeholder', 'placeholder', b('bold text'))
+function getCodeFromBlock(workspace, generator, name) {
+    const block = workspace.newBlock(name)
+    block.initSvg()
+    block.render()
+    const py = generator.blockToCode(block)
+    block.dispose
+    return py
+}
 
-    return [SomeContent]
+function createEntry(workspace, entry) {
+    const entryDiv = div();
+    entryDiv.appendChild(tag("p", `${getCodeFromBlock(workspace, pythonGenerator, entry.name)}`));
+    entryDiv.appendChild(tag("h4", `${entry.description}`));
+    entryDiv.appendChild(hr());
+    return entryDiv; 
+}
+
+function createSection(workspace, block) {
+    const sectionDiv = div();
+    sectionDiv.append(tag("h2", `${block.name}`));
+    for (let j=0; j<block.entries.length; j++) {
+        sectionDiv.appendChild(createEntry(workspace, block.entries[j]));
+    }
+    return sectionDiv;
+}
+
+export default function (toolbox) {    
+    const mainDiv = div()
+    const BlocklyCanvas = div({ id: 'block-canvas' })
+    BlocklyCanvas.style.display = "none"
+    const workspace = inject(BlocklyCanvas, {
+            toolbox: toolbox,
+    })
+    pythonGenerator.init(workspace);
+    console.log(blocks);
+    // TODO: Refactor this into multiple functions,
+    // Remember to npx webpack before reloading
+    /* python -m http.server -> http://localhost:8000/?cheatsheet*/
+    for (let i=0; i<blocks.length; i++) {
+        mainDiv.append(createSection(workspace, blocks[i]));
+    }
+   return [mainDiv];
 }
