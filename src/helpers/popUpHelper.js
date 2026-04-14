c82c6a1d44dbc622e0f30ee948bcb9cbffed4410
TheAngryGhost <theangryghost@protonmail.com>
2026-04-13 19:51:14 -0500

    added popup support needs to be checked still

--- src/helpers/popUpHelper.js
+++ src/helpers/popUpHelper.js
@@ -1,28 +1,45 @@
-import { button, on, shadow, slot, tag } from 'ellipsi'
+import { button, div, img, on, tag } from 'ellipsi'
 
 export const closePopUpEvent = 'close-pop-up'
 
 export const PopUp = (...children) => {
-    const CloseButton = button(
-        { part: 'close-button' },
-        'x',
-        on('click', () => PopUp.dispatchEvent(new Event(closePopUpEvent))),
-    )
-
-    const PopUp = tag(
-        'pop-up',
-        children,
-        shadow(CloseButton, slot()),
-        on(closePopUpEvent, () => PopUp.remove()),
-    )
-
-    document.addEventListener('keydown', (event) => {
-        if (event.key !== 'Escape') {
+    const closePopUp = () => {
+        if (Dialog.open) {
+            Dialog.close()
             return
         }
 
-        PopUp.dispatchEvent(new Event(closePopUpEvent))
+        Dialog.remove()
+    }
+
+    const CloseButton = button(
+        img({
+            src: '/images/cancel.svg',
+            alt: 'Close dialog',
+        }),
+        { type: 'button', class: 'popup-close-button' },
+        on('click', () => Dialog.dispatchEvent(new Event(closePopUpEvent))),
+    )
+
+    const Dialog = tag(
+        'dialog',
+        { class: 'popup-dialog' },
+        div(
+            { class: 'popup-content' },
+            CloseButton,
+            children,
+        ),
+        on(closePopUpEvent, closePopUp),
+        on('close', () => Dialog.remove()),
+    )
+
+    queueMicrotask(() => {
+        if (!Dialog.isConnected || Dialog.open) {
+            return
+        }
+
+        Dialog.showModal()
     })
 
-    return PopUp
+    return Dialog
 }
